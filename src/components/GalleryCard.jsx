ff08c433b79042eadda25e1a08b3d115a483b3bc
samantha-marsee <sam572@cornell.edu>
2025-02-10 15:30:52 +0000

    bg color of card

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -2,10 +2,10 @@ import Citation from "./Citation";
 
 export default function GalleryCard({ imgUri, altText, citation, caption, color }) {
   return (
-    <div className="card">
+    <div className="card" style = {{backgroundColor: color }}>
       <img src={imgUri} alt={altText} />
 
-      <div className="caption" style = {{backgroundColor: color }}>
+      <div className="caption">
         <p style={{ borderBottom: "1px solid #000000", paddingBottom: "2px"}}>
           {caption}
         </p>
