dd041349d37f2a9fda24e1187b471224097744c2
samantha-marsee <sam572@cornell.edu>
2025-02-10 15:24:11 +0000

    using align prop

--- src/components/Citation.jsx
+++ src/components/Citation.jsx
@@ -1,6 +1,6 @@
 export default function Citation({ citation, align }) {
   return (
-    <p className="citation" style={{ textAlign: 'left' }}>
+    <p className="citation" style={{ textAlign: align }}>
       Source: <cite>{citation}</cite>
     </p>
   )
